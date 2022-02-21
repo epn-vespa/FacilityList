6f3ac4ed4a73ea821eaab8ce32e91bd74cdc9c0e
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-02-21 22:47:48 +0100

    Update IAU-MPC.py

--- data/IAU-MPC/IAU-MPC.py
+++ data/IAU-MPC/IAU-MPC.py
@@ -1,30 +1,26 @@
 import json
 
-file= '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.txt'
+file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.txt'
+output_file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.json'
 
-print(file)
-dict = {}
-
-fields= ['Code', 'Longitude', 'cos', 'sin', 'Name',]
-l = 1
+my_list=[]
 with open(file) as f:
     for line in f:
-       description = list(line.strip().split())
-       #print(description)
-       print("code :", description[0])
-       print("Longitude : ", description[1])
-       print("cos : ", description[2])
-       print("sin : ", description[3])
-       print("Name : ", description[4])
-       
-#       i = 0
-#      dict2 = {}
-#       while i<len(fields):
-#            dict2[fields[i]]= description[i]
-#            i = i + 1
-#            dict.update(dict2)
-#            l = l + 1
+        description = list(line.strip().split())
+        dic_line ={ 
+            "code" : description[0],
+            "Longitude": description[1],
+            "cos" : description[2],
+            "sin" : description[3],
+            "Name": description[4]
+            }
+            
+        my_list.append(dic_line)
+        print(json.dumps(dic_line, indent=4))
+
+
+with open(output_file,"w") as out_f:
+    out_f.write(json.dumps(my_list, indent=4))
+
         
-out_file = open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.json', "w")
-json.dump(dict, out_file, indent = 4)
-out_file.close()
+
