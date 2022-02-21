9f1f8bb5b6a85e005a28c3ee5ae39ce71bb62740
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-02-21 22:54:28 +0100

    Update IAU-MPC.py

--- data/IAU-MPC/IAU-MPC.py
+++ data/IAU-MPC/IAU-MPC.py
@@ -4,8 +4,13 @@ file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.tx
 output_file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.json'
 
 my_list=[]
+#first line flag
+first_line_flag= True
 with open(file) as f:
     for line in f:
+        if first_line_flag :
+            first_line_flag = False
+            continue
         description = list(line.strip().split())
         dic_line ={ 
             "code" : description[0],
@@ -16,7 +21,7 @@ with open(file) as f:
             }
             
         my_list.append(dic_line)
-        print(json.dumps(dic_line, indent=4))
+        #print(json.dumps(dic_line, indent=4))
 
 
 with open(output_file,"w") as out_f:
