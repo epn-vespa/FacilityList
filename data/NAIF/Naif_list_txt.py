539ff4d6b1de27e2745165547e5e4eb9e74ebe7e
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-02-23 12:02:06 +0100

    Create Naif_list_txt.py

---
+++ data/NAIF/Naif_list_txt.py
@@ -0,0 +1,27 @@
+import json
+
+file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/NAIF/naif.txt'
+output_file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/NAIF/naif.json'
+
+my_list = []
+# first line flag
+first_line_flag = True
+with open(file) as f:
+    for line in f:
+        if first_line_flag:
+            first_line_flag = False
+            continue
+        description = list(line.strip().split())
+        dic_line = {
+            "code": description[0],
+            "Name": description[1]
+        }
+
+        my_list.append(dic_line)
+        # print(json.dumps(dic_line, indent=4))
+
+with open(output_file, "w") as out_f:
+    out_f.write(json.dumps(my_list, indent=4))
+
+
+
