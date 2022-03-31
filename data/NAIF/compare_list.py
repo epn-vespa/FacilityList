fe4dd4647386dbdbe17ffae45a1dd3eaf90337b9
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-03-31 10:27:22 +0200

    Update compare_list.py

--- data/NAIF/compare_list.py
+++ data/NAIF/compare_list.py
@@ -11,7 +11,7 @@ with open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/NAIF/NAIF.json'
 with open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/WIKIDATA/list_observatories_spacecrafts.json') as f:
     wikidata = json.load(f)
 
-results=[]
+results = []
 tres_certain = []
 tres_probable = []
 probable = []
@@ -24,10 +24,10 @@ for i,e in enumerate(data_aas):
 
     def mon_scorer(q, c):
         r = fuzz.WRatio(q['Name'], c['itemLabel']) + fuzz.WRatio(q['Name'], c['aliases'])
-        if c['all_NAIF_ID'] != "" :
-            if q['ID'] == c['all_NAIF_ID'] :
+        if c['all_NAIF_ID'] != "":
+            if q['ID'] == c['all_NAIF_ID']:
                 r += 500
-            else :
+            else:
                 r -= 100
         return r
 
@@ -38,15 +38,15 @@ for i,e in enumerate(data_aas):
 
 
     results.append(r)
-    trouve = False;
+    trouve = False
     for r_elem in r:
         print("    " + str(r_elem[1]) + " : " + str(r_elem[0]))
-        if r_elem[1] > 500 :
+        if r_elem[1] > 500:
             trouve = True
             tres_certain.append((e, r_elem[0]))
         elif r_elem[1] > 180:
             tres_probable.append((e, r_elem[0]))
-        elif r_elem[1] > 150 :
+        elif r_elem[1] > 150:
             probable.append((e, r_elem[0]))
     if not trouve : non_trouves.append(e)
 
@@ -55,11 +55,11 @@ print("tres_probable : " + str(len(tres_probable)))
 print("probable : " + str(len(probable)))
 print("non_trouves : " + str(len(non_trouves)))
 
-with open("tres_certain.json",'w') as fout :
+with open("tres_certain.json",'w') as fout:
     fout.write(json.dumps(tres_certain, indent=4))
-with open("tres_probable.json",'w') as fout :
+with open("tres_probable.json",'w') as fout:
     fout.write(json.dumps(tres_probable, indent=4))
-with open("probable.json",'w') as fout :
+with open("probable.json",'w') as fout:
     fout.write(json.dumps(probable, indent=4))
-with open("non_trouves.json",'w') as fout :
+with open("non_trouves.json",'w') as fout:
     fout.write(json.dumps(non_trouves, indent=4))
