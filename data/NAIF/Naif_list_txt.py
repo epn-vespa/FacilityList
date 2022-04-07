8aea4290c6cdfff57a95021ffc4520f72ea13551
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-04-07 15:04:07 +0200

    Update Naif_list_txt.py

--- data/NAIF/Naif_list_txt.py
+++ data/NAIF/Naif_list_txt.py
@@ -11,9 +11,9 @@ with open(file) as f:
         if first_line_flag:
             first_line_flag = False
             continue
-        description = list(line.strip().split())
+        description =" ".join(line.strip().split()).split("'")
         dic_line = {
-            "code": description[0],
+            "code": description[0].strip(),
             "Name": description[1]
         }
 
