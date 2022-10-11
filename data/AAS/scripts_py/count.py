7affa02053fbe7d9b1083007731f13aa6eac47b7
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2022-10-11 11:08:39 +0200

    update scripts

---
+++ data/AAS/scripts_py/count.py
@@ -0,0 +1,5 @@
+import json
+with open('AAS.json', 'r') as f:
+    print(len(json.load(f)))
+
+
---
+++ data/IAU-MPC/scripts_py/extract_data_IAU-MPC.py
@@ -0,0 +1,32 @@
+import json
+
+file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/datas/IAU-MPC.txt'
+output_file = '/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/IAU-MPC.json'
+
+my_list=[]
+#first line flag
+first_line_flag= True
+with open(file) as f:
+    for line in f:
+        if first_line_flag :
+            first_line_flag = False
+            continue
+        description = list(line.strip().split(maxsplit=4)
+)
+        dic_line ={ 
+            "code" : description[0],
+            #"Longitude": description[1],
+            #"cos" : description[2],
+            #"sin" : description[3],
+            "Name": description[4]
+            }
+            
+        my_list.append(dic_line)
+        #print(json.dumps(dic_line, indent=4))
+
+
+with open(output_file,"w") as out_f:
+    out_f.write(json.dumps(my_list, indent=4))
+
+        
+
