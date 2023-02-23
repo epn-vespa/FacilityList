c4a24df8e5f4369fddb96abb480b1a5465430aa6
LauraD12 <98822880+LauraD12@users.noreply.github.com>
2023-02-23 16:52:32 +0100

    update

--- data/IAU-MPC/scripts/compare_list_IAU_wiki.py
+++ data/IAU-MPC/scripts/compare_list_IAU_wiki.py
@@ -1,15 +1,14 @@
-### Compare les listes en utilisant le module fuzzy
+#############################################################################################################################
+# Compare strings of characters between the list IAU_MPC and a list of items extract of wikidata with fuzzywuzzy library ####
+#############################################################################################################################
 
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-
+from multiprocessing import Pool
 import json
-
 import cProfile
 
-from multiprocessing import Pool
-
-
+#Takes as input a query object and a candidate object and returns a score indicating the similarity between their names and IAU identifiers
 def mon_scorer(q, c):
     r = fuzz.WRatio(q['Name'], c['itemLabel']) + fuzz.WRatio(q['Name'], c['aliases']) + fuzz.WRatio(q['ID'], c['all_Minor_Planet_Center_observatory_ID'])
     if c['all_Minor_Planet_Center_observatory_ID'] != "":
@@ -25,9 +24,10 @@ def dummy_proc(x):
 with open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/IAU-MPC/datas/IAU-MPC.json') as f:
     data_iau = json.load(f)
 
-with open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/WIKIDATA/Scripts/extract_wikidata.json') as f:
+with open('/Users/ldebisschop/Documents/GitHub/FacilityList/data/WIKIDATA/scripts/extract_wikidata.json') as f:
     wikidata = json.load(f)
 
+# Search for the best match based on the custom scoring function
 def get_scores( t ):
     i=t[0]
     e=t[1]
@@ -36,7 +36,9 @@ def get_scores( t ):
     print("  " + str(r[0][1]) + " : " + str(r[0][0]))
     return r
 
-
+#Initializes several empty lists to store the results of the comparison.
+#Use the "multiprocessing" libray to run the "get_scores"  fucntion in parallel for each element of "data_iau"
+#The results are stored in the "results" list
 def compare_iau(data_iau, wikidata):
     results = []
     tres_certain = []
@@ -47,6 +49,8 @@ def compare_iau(data_iau, wikidata):
     with Pool(8) as p:
         results = p.map(get_scores, enumerate(data_iau) )
 
+# if the score of the best match is higher than certain threshold, the pair is adde to the " tres_certain" list.
+# if no matches are found, the element is ades to the "non_trouves" list.
     for i, e in enumerate(data_iau):
         r = results[i]
         trouve = False
@@ -65,12 +69,13 @@ def compare_iau(data_iau, wikidata):
     print("probable : " + str(len(probable)))
     print("non_trouves : " + str(len(non_trouves)))
 
+#the fuction writes the results to seprarate JSON files and pritns the number of elements in each list.
     with open("tres_certain.json", 'w') as fout:
         fout.write(json.dumps(tres_certain, indent=4))
-    with open("tres_probable.json", 'w') as fout:
-        fout.write(json.dumps(tres_probable, indent=4))
-    with open("probable.json", 'w') as fout:
-        fout.write(json.dumps(probable, indent=4))
+    #with open("tres_probable.json", 'w') as fout:
+    #    fout.write(json.dumps(tres_probable, indent=4))
+    #with open("probable.json", 'w') as fout:
+    #    fout.write(json.dumps(probable, indent=4))
     with open("non_trouves.json", 'w') as fout:
         fout.write(json.dumps(non_trouves, indent=4))
     with open("results.json", 'w') as fout:
