6835c18e8a32cb4ba418a28d404fe610014338b5
Cecconi Baptiste <baptiste.cecconi@obspm.fr>
2024-11-20 10:46:23 +0100

    Harmonize directory name

---
+++ data/NAIF/script/extract_data_Naif_list_txt.py
@@ -0,0 +1,30 @@
+import json
+from pathlib import Path
+
+data_dir = Path(__file__).parents[1] / "data"
+
+file = data_dir / 'naif.txt'
+output_file = data_dir / 'naif.json'
+
+my_list = []
+# first line flag
+first_line_flag = True
+with open(file) as f:
+    for line in f:
+        if first_line_flag:
+            first_line_flag = False
+            continue
+        description =" ".join(line.strip().split()).split("'")
+        dic_line = {
+            "code": description[0].strip(),
+            "Name": description[1]
+        }
+
+        my_list.append(dic_line)
+        # print(json.dumps(dic_line, indent=4))
+
+with open(output_file, "w") as out_f:
+    out_f.write(json.dumps(my_list, indent=4))
+
+
+
