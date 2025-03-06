87f807af5c1bcd7401e6c03821b12721b4716fe8
Sazuna <liza.fretel@gmail.com>
2025-03-06 12:01:41 +0100

    Graph add method & OntologyMapping class for dict/RDF compatibility

--- src/data_updater/graph.py
+++ src/data_updater/graph.py
@@ -1,26 +1,83 @@
 """
-Instanciate a graph only once as a singleton for all other classes.
-The Graph class replaces rdflib.Graph and reuses Graph's attributes.
-It stores the Observation Facilities namespace in the class variable OBS.
-
-Usage:
-    from graph import Graph
-    g = Graph() # instanciate
-    obs = g.OBS # get Observation Facility namespace
-
-    # Then, use g as a rdflib graph object:
-    g.add((g.OBS["subj"], URIRef("predicate"), URIRef("obj")))
-
 Author:
     Liza Fretel (liza.fretel@obspm.fr)
 """
-from rdflib import Graph as G, Namespace
+
+from typing import Type, Union
+from rdflib import Graph as G, Namespace, Literal, URIRef, Node
+from rdflib.namespace import RDF, SKOS
 import warnings
 
+class OntologyMapping():
+    """
+    Metadata mapping between Ontology standards and the extracted dictionaries.
+    Use this class for quick dictionary to RDF conversion.
+    """
+
+    # TODO remove
+    #_OBS = Namespace("http://semanticweb.org/obspm/ontologies/2025/2/VO-schema/obsfacilities")
+
+    _MAPPING = {
+        "type": RDF.type,
+        "label": SKOS.prefLabel,
+        "definition": SKOS.definition,
+        "alt_label": SKOS.altLabel,
+        "uri": URIRef,
+        "wavelength": Graph._OBS.wavelength # DataProperty
+    }
+
+    #_REVERSE_MAPPING = {v: k for k, v in _MAPPING.items()}
+
+    def __init__(
+            self,
+            graph: Graph):
+        self._graph = graph
+
+    @property
+    def graph(self):
+        return self._graph
+
+    def convert_attr(
+            self,
+            attr: Union[str, Node]):
+        """
+        Convert an attribute accordingly to the mapping.
+        For example, if attribute is "definition", returns SKOS.definition.
+
+        Keyword arguments:
+        attr -- the attribute to convert
+        """
+        if type(attr) == str:
+            return OntologyMapping._MAPPING.get(
+                    attr,
+                    Graph._OBS[attr])
+        else:
+            return attr
+            # return OntologyMapping._REVERSE_MAPPING.get(attr, None)
+
+    def __getattr__(
+            self,
+            attr):
+        return self.convert_attr(attr)
+
 class Graph():
+    """
+    Instanciate a rdflib Graph as a singleton to prevent multiple
+    instantiation. Replace rdflib.Graph and reuses rdflib.Graph's attributes.
+    Stores the Observation Facilities namespace in the class variable OBS.
+
+    Usage:
+        from graph import Graph
+        g = Graph() # instanciate
+        obs = g.OBS # get Observation Facility namespace
+
+        # Then, use g as a rdflib graph object:
+        g.add((g.OBS["subj"], URIRef("predicate"), URIRef("obj")))
+    """
     _graph = None
     _OBS = Namespace("http://semanticweb.org/obspm/ontologies/2025/2/VO-schema/obsfacilities")
-    _warned = False
+    _OM = OntologyMapping()
+    _warned = False # Warn only once for multiple instantiation.
 
     def __init__(self, filename = ""):
         if Graph._graph is not None:
@@ -29,7 +86,7 @@ class Graph():
                 Graph._warned = True
             return
         Graph._graph = G()
-        self.bind("obs", Graph._OBS)
+        self.bind("obs", self.OBS)
         if filename:
             Graph._graph.parse(filename)
             # init graph
@@ -42,16 +99,55 @@ class Graph():
     def OBS(self):
         return Graph._OBS
 
+    @property
+    def OM(self):
+        return Graph._OM
+
     def __call__(self):
         return Graph._graph
 
     def __getattr__(self, attr):
         return getattr(Graph._graph, attr)
 
+    def add(
+            self,
+            params: Tuple[str, str, str]
+            objtype: Type = Literal,
+            source: Union[URIRef, str] = None):
+        """
+        Add a RDF triple to the graph.
+        Override rdflib's Graph.add() method.
+        For conversion from a dictionary, the object type has to be specified,
+        as an object can be an ontological reference (URIRef) or a value.
+
+        Keyword arguments:
+        subj -- the subject of the triple.
+        predicate -- the predicate of the triple.
+        obj -- the object of the triple.
+        objtype -- the type of the object (Literal, URIRef...)
+        source -- the origin of the added triple (URL, URI...)
+        """
+        subj = params[0]
+        predicate = params[1]
+        obj = params[2]
+
+        if type(subj) == str:
+            subj_uri = self.OBS[subj]  # Convert subject to URI with _OBS
+
+        if type(predicate) == str:
+            predicate = self.OM.convert_attr(predicate)
+
+        if predicate is None:
+            raise ValueError(f"Predicate can not be None.")
+
+        obj_value = objtype(obj)
+
+        self._graph.add((subj_uri, predicate, obj_value))
+
 if __name__ == "__main__":
-    from rdflib import URIRef
     g = Graph()
     g.add((g.OBS["Liza"], URIRef("eats"), URIRef("pizza")))
     g2 = Graph() # should print a warning
+    # g and g2 point to the same Graph object (singleton)
     g2.add((g.OBS["Laura"], URIRef("drinks"), URIRef("coffee")))
-    print(g.serialize()) # should print 
+    print(g.serialize()) # both g and g2 have Laura
