3a256f71a3a4f6a64b1cfc1c56433c1dc5ff2cd2
Sazuna <liza.fretel@obspm.fr>
2025-03-12 17:14:14 +0100

    Debug utils' del_aka : append the rest of the string to the label after the aka

--- src/data_updater/utils.py
+++ src/data_updater/utils.py
@@ -11,6 +11,7 @@ def standardize_uri(label: str) -> str:
     label -- the label of the entity.
     """
     label = label.lower()
+    # label = del_aka(label)
     label = re.sub(r"[^\w\s\.]", ' ', label)
     label = re.sub(r"\s+", ' ', label) # Remove multiple spaces
     label = label.split(' ')
@@ -18,21 +19,36 @@ def standardize_uri(label: str) -> str:
     label = quote(label)
     return label
 
-def del_aka(label: str) -> str:
+def del_aka(label: str) -> Tuple[str]:
     """
     Delete stopwords like 'aka' from the label.
     """
-    stopwords = '|'.join(["aka", "also known as", "formerly the", "formerly"])
+    stopwords = '|'.join(["aka",
+                          "a.k.a.",
+                          "also known as",
+                          "formerly the",
+                          "formerly"])
 
     exp = re.compile(f"\\b({stopwords})\\b")
     for match in re.finditer(exp, label):
         start_index = match.start()
         end_index = match.end()
-        if label[start_index-1] != '(': # The acronym is not between ()
-            # Add () around the acronym
-            label = label[:start_index] + " (" + label[start_index:] + ")"
+        if label[start_index-1] != '(': # The aka is not between ()
+            # Add () around the aka
+            after_aka = label[start_index:]
+            # The aka end after a ','
+            comma = after_aka.find(',')
+            if comma:
+                aka = after_aka[0:comma]
+                after_aka = after_aka[comma+1:]
+            else:
+                aka = after_aka
+                after_aka = ""
+            label = f"{label[:start_index]} ({aka}) {after_aka}"
     label = re.sub(exp, "", label)
-    label = re.sub(" +", " ", label)
+    print("ici:", label)
+    label = re.sub(r" +", " ", label)
+    label = re.sub(r"\( ", "(", label)
     return label.strip()
 
 def cut_acronyms(label: str) -> Tuple[str]:
@@ -43,6 +59,7 @@ def cut_acronyms(label: str) -> Tuple[str]:
     Keyword arguments:
     label -- the label containing acronyms
     """
+    label = del_aka(label)
     label = label.strip()
     acronyms = list(re.finditer(r"\([^(]+?\)", label))
     if not acronyms:
@@ -57,6 +74,7 @@ def cut_acronyms(label: str) -> Tuple[str]:
         acronym_str = acronym.group()[1:-1].strip() # remove ()
         full_name_without_acronyms += name + " "
 
+    full_name_without_acronyms += label[prev_acronym_idx:]
     if label[-1] != ')':
         acronym_str = "" # Acronym for the whole string (last word)
     # Return full name without acronyms + the last acronym
@@ -71,3 +89,6 @@ def get_alternate_name(label: str) -> list:
     When there is 'and' or 'or' in a label, it may be an alternate name.
     """
     # TODO
+
+if __name__ == "__main__":
+    pass
