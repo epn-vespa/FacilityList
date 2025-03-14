ecf494634d6708ced9479fd833b6d4d6b2579a66
Sazuna <liza.fretel@gmail.com>
2025-03-14 18:15:09 +0100

    Update SPASE extractor: create relations isPartOf & scrap all json's links from the JS tag on github page.

--- src/data_updater/extractor/spase_extractor.py
+++ src/data_updater/extractor/spase_extractor.py
@@ -22,11 +22,12 @@ from bs4 import BeautifulSoup
 from extractor.cache import CacheManager
 import json
 from utils import clean_string, extract_items
+import re
 
 class SpaseExtractor():
 
     # URL = "https://heliophysicsdata.gsfc.nasa.gov/websearch/dispatcher?action=CDAW_ELEMENT_LIST_PANE_ACTION&element="
-    # URL = "https://hpde.io/SMWG/Observatory/index.html" # old version of the code (2025.03.14)
+    # URL = "https://hpde.io/SMWG/Observatory/index.html" # first version of the code (2025.03.14)
     URL = "https://github.com/spase-group/spase-info/tree/master/SMWG/Observatory/"
 
     # URI to save this source as an entity
@@ -45,7 +46,7 @@ class SpaseExtractor():
                       #"URL": "url",
                       "AlternateName": "alt_label",
                       "ObservatoryRegion": "location",
-                      "ObservatoryGroupId": "part_of",
+                      "ObservatoryGroupID": "part_of",
                       #"Aknowledgement": "",
                       "Latitude": "latitude",
                       "Longitude": "longitude",
@@ -53,18 +54,29 @@ class SpaseExtractor():
                       "EndDate": "end_date"}
 
     def _get_all_links(self,
-                      url: str) -> List:
+                       url: str) -> List:
         """
         Get a list of all the content links (json files) from the
         SPASE github repository, recursively.
         """
         links = set()
         content = CacheManager.get_page(url)
+
         if not content:
             return links
+        # Get directories
         soup = BeautifulSoup(content, "html.parser")
         a = soup.find_all("a", attrs = {"class":"Link--primary"})
         hrefs = list(set([href.attrs["href"] for href in a]))
+
+        # Get files at root
+        #if url == SpaseExtractor.URL:
+        # The files are in the <script> and not in the html itself.
+        root_links = re.findall(r'"path":"([^:]*?\.json)"', content)
+        for link in root_links:
+            link = "https://raw.githubusercontent.com/spase-group/spase-info/refs/heads/master/" + link
+            links.add(link)
+
         for href in hrefs:
             if href.endswith(".json"):
                 href = href.replace("blob", "refs/heads")
@@ -86,7 +98,9 @@ class SpaseExtractor():
         """
         result = dict()
         hrefs = self._get_all_links(SpaseExtractor.URL)
+
         if not hrefs:
+            # No links found
             return dict()
 
         for href in hrefs:
@@ -113,6 +127,14 @@ class SpaseExtractor():
                         alt_labels.add(value)
                     elif key == "description" and "description" in data:
                             continue # Only one description per entity
+                    elif key == "part_of":
+                        # Save the observatory group
+                        observatory_group = value.split('/')[-1]
+                        if observatory_group not in result:
+                            result[observatory_group] = dict()
+                        result[observatory_group]["label"] = observatory_group
+                        result[observatory_group]["code"] = value
+                        data[key] = observatory_group
                     elif key in data:
                         data[key].append(value)
                     else:
@@ -122,13 +144,14 @@ class SpaseExtractor():
             if alt_labels:
                 data["alt_label"] = alt_labels
 
+            # url
+            data["url"] = href
+
             # label
             if not data["label"]:
                 data["label"] = href.split('/')[-1]
-            result[data["label"]] = data
 
-            # url
-            data["url"] = href
+            result[data["label"]] = data
         return result
 
 if __name__ == "__main__":
