b32b58f3786b90948734a52b297c68bcc62f6fbb
Sazuna <liza.fretel@gmail.com>
2025-05-19 18:52:52 +0200

    Test candidate pair: test iter method

--- test/test_candidate_pair_methods.py
+++ test/test_candidate_pair_methods.py
@@ -40,5 +40,27 @@ class TestCandidatePairMethods(unittest.TestCase):
         CPM.del_candidate_pairs(e2)
         CPM.del_candidate_pairs(f1)
 
+    def test_iter(self):
+        CPM = CandidatePairsMapping(AasExtractor(), PdsExtractor())
+
+        e1 = Entity("E1")
+        e2 = Entity("E2")
+        e3 = Entity("E3")
+        f1 = Entity("F1")
+        f2 = Entity("F2")
+
+        CPM._list1_indexes = [e1, e2, e3]
+        CPM._list2_indexes = [f1, f2]
+        CPM._mapping = [
+            [CandidatePair(e1, f1), CandidatePair(e1, f2)],
+            [CandidatePair(e2, f1), CandidatePair(e2, f2)],
+            [None, CandidatePair(e3, f2)],
+        ]
+
+        for i, (_, _, cp) in enumerate(CPM.iter_mapping()):
+            if i == 1:
+                assert cp.member1 == e2 and cp.member2 == f2
+        assert i == 4
+
 if __name__ == "__main__":
     unittest.main()
