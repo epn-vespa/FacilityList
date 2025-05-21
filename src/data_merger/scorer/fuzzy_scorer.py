2b53771258abee125d790abad143c9c6ec54ff95
Sazuna <liza.fretel@gmail.com>
2025-05-21 17:06:58 +0200

    Fuzzy scorer: replace Fuzzywuzzy by rapidfuzz

--- src/data_merger/scorer/fuzzy_scorer.py
+++ src/data_merger/scorer/fuzzy_scorer.py
@@ -4,19 +4,25 @@ and alt label into consideration. If there is a perfect match,
 return 1. Fuzzy score computes a similarity between two strings,
 or an edit-distance. We use the Levenshtein distance here.
 
+Troubleshooting:
+    fuzzywuzzy is a very slow library, and Wikidata has a lot of
+    alt labels.
+    1. Only keep one alt label
+    2. Use rapidfuzz instead
+
 Author:
     Liza Fretel (liza.fretel@obsmp.fr)
 """
 
 
-from typing import Union
-from fuzzywuzzy import fuzz
+from typing import List, Union
+# from fuzzywuzzy import fuzz as wuzz
+from rapidfuzz import fuzz, utils
 
 from graph import Graph
 from data_merger.entity import Entity
 from data_merger.scorer.score import Score
 from data_merger.synonym_set import SynonymSet
-from utils.utils import remove_punct
 from utils.performances import timeall
 
 
@@ -41,17 +47,28 @@ class FuzzyScorer(Score):
         """
         highest_score = 0
 
-        labels1 = entity1.get_values_for("alt_label")
-        labels1.update(entity1.get_values_for("label"))
-        labels2 = entity2.get_values_for("alt_label")
-        labels2.update(entity2.get_values_for("label"))
+        labels1 = entity1.get_values_for("label")
+        labels1.update(entity1.get_values_for("alt_label"))
+        labels2 = entity2.get_values_for("label")
+        labels2.update(entity2.get_values_for("alt_label"))
         for label1 in labels1:
             for label2 in labels2:
+                """
                 # partial: ignore punctuation?
                 # token sort: different word order does not impact
-                # ignore case (lower())
-                score = fuzz.token_sort_ratio(remove_punct(label1.lower()),
+                # FuzzyWuzzy
+                # Total: 273.05 seconds for aas/nssdc
+                score = wuzz.token_sort_ratio(remove_punct(label1.lower()),
                                               remove_punct(label2.lower()))
+                """
+
+                # RapidFuzz
+                score = fuzz.token_sort_ratio(label1,
+                                              label2,
+                                              processor = utils.default_process)
+
+                if score == 100:
+                    return 1.0
                 if score > highest_score:
                     highest_score = score
         return highest_score / 100
