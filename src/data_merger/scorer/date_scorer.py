b7b4591696da946f5735275906b104af24e44a44
Sazuna <liza.fretel@gmail.com>
2025-05-21 16:36:37 +0200

    Date scorer: only compare year

--- src/data_merger/scorer/date_scorer.py
+++ src/data_merger/scorer/date_scorer.py
@@ -1,7 +1,3 @@
-"""
-Verify that dates are the same between two entities.
-"""
-
 from typing import Union
 from data_merger.entity import Entity
 from data_merger.scorer.score import Score
@@ -15,7 +11,6 @@ class DateScorer(Score):
     # Name of the score computed by this class (as in score.py)
     NAME = "date"
 
-
     @timeall
     def compute(graph: Graph,
                 entity1: Union[Entity, SynonymSet],
@@ -27,28 +22,41 @@ class DateScorer(Score):
         If no incompatibility was found, return -1.
         If an incompatibility was found, return -2.
         """
-        ld_e1 = entity1.get_values_for("launch_date")
-        ld_e2 = entity2.get_values_for("launch_date")
-        #ld_e1 = {x.date() for x in ld_e1}
-        #ld_e2 = {x.date() for x in ld_e2}
-        if ld_e1 and ld_e2 and not DateScorer._compare_years(ld_e1, ld_e2):#ld_e1.intersection(ld_e2):
-            return -2
-        sd_e1 = entity1.get_values_for("start_date")
-        sd_e2 = entity2.get_values_for("start_date")
-        if sd_e1 and sd_e2 and not DateScorer._compare_years(sd_e1, sd_e2):# sd_e1.intersection(sd_e2):
-            return -2
-        ed_e1 = entity1.get_values_for("end_date")
-        ed_e2 = entity2.get_values_for("end_date")
-        if ed_e1 and ed_e2 and not DateScorer._compare_years(ed_e1, ed_e2):#ed_e1.intersection(ed_e2):
-            return -2
-        return -1
+        # Check all relevant date fields
+        date_attrs = ["launch_date", "start_date", "end_date"]
+        for attr in date_attrs:
+            if not DateScorer._compare_entity_dates(entity1,
+                                                    entity2,
+                                                    attr):
+                return -2 # Return -2 if any incompatibility is found
+        return -1 # Return -1 if no incompatibility
 
+    @staticmethod
+    def _compare_entity_dates(entity1: Union[Entity, SynonymSet],
+                              entity2: Union[Entity, SynonymSet],
+                              attr: str) -> bool:
+        """
+        Compare the year of the dates for a given field in both entities.
+        Return True if dates are compatible, False if not.
+        """
+        dates1 = entity1.get_values_for(attr)
+        dates2 = entity2.get_values_for(attr)
+
+        # Only compare if both sets have dates
+        if dates1 and dates2:
+            return DateScorer._compare_years(dates1, dates2)
+        return True  # If one or both sets are empty, we assume they are compatible.
+
+    @staticmethod
     def _compare_years(dates1: set,
                        dates2: set) -> bool:
         """
         If any date from dates1 & dates2 are the same year,
-        return True
+        return True.
         """
+        years1 = {date1.year for date1 in dates1 if date1 is not None}
+        years2 = {date2.year for date2 in dates2 if date2 is not None}
+        return not years1.isdisjoint(years2)
         for date1 in dates1:
             if date1 is None:
                 continue
@@ -58,8 +66,5 @@ class DateScorer(Score):
                     continue
                 year2 = date2.year
                 if year1 == year2:
-                    # Same year.
-                    # TODO: compare months (sometimes 1st of January is for an
-                    # empty date)
-                    return True
-        return False
+                    return True # Same year found
+        return False # No matching year found
