d3740dbbde6a9c5cb8dff23787a2b51098e25f32
Sazuna <liza.fretel@gmail.com>
2025-12-11 16:45:36 +0100

    remove prints

--- src/utils/stopwords.py
+++ src/utils/stopwords.py
@@ -2,7 +2,6 @@ import pathlib
 from pathlib import Path
 
 stopwords_folder = pathlib.Path(__file__).parent.parent.parent / "data" / "stopwords"
-print(stopwords_folder)
 
 all_stopwords = dict()
 
@@ -11,7 +10,6 @@ for file in stopwords_folder.glob("*"):
     if not "README" in str(filename):
         with open(file, "r") as f:
             all_stopwords[filename] = f.read().split('\n')
-print(all_stopwords)
 
 def words(lang):
     return all_stopwords[lang]
