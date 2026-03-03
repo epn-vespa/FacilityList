a5800e173264274f3c2752c998648918a3654465
Sazuna <liza.fretel@gmail.com>
2026-03-03 15:11:56 +0100

    Save views in the folder of the input ontology

--- src/generate_views.py
+++ src/generate_views.py
@@ -12,10 +12,16 @@ Author:
 import argparse
 from views import merge_uris, split_instruments, generate_csv_json
 from graph.graph import Graph
+from pathlib import Path
 
 
 def main(input_ontology: str,
          output_merged: str):
+    output_merged_folder = Path(input_ontology)
+    output_merged = output_merged_folder.parent / output_merged
+    if output_merged.exists():
+        raise FileExistsError(f"{output_merged} already exists. Please use another output filename.")
+    output_merged = str(output_merged)
     merge_uris.main(input_ontology,
                     output_merged)
     output_obsf, output_instr = split_instruments.split_instruments(output_merged)
@@ -37,8 +43,8 @@ if __name__ == "__main__":
                         "--outout-ontology",
                         dest = "output_ontology",
                         required = False,
-                        default = "output.ttl",
-                        help = "Base output ontology filename that will contain all merged entities (intermediate step to merge synonym sets before generating views).")
+                        default = "merged.ttl",
+                        help = "Base output ontology filename that will contain all merged entities (intermediate step to merge synonym sets before generating views). Note that the views will be generated next to the original ontologies.")
     args = parser.parse_args()
     main(args.input_ontology,
          args.output_ontology)
