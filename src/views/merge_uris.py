c3554fc8e95b49741eece9b435a133eab5ff4636
Sazuna <liza.fretel@gmail.com>
2026-06-22 15:40:31 +0200

    Merge_uris: use Value instead of tuples + add Blank nodes & provenance to literals

--- src/views/merge_uris.py
+++ src/views/merge_uris.py
@@ -1,5 +1,6 @@
 """
-Generate a public ontology from the output of disambiguation.
+Generate an intermediate ontology from the output of disambiguation.
+This ontology contains both facilities & instruments.
 
 Author:
     Liza Fretel (liza.fretel@obspm.fr)
@@ -9,13 +10,16 @@ import argparse
 from graph.graph import Graph
 from graph.properties import Properties
 from graph.entity import Entity
+from graph.value import Value
 from graph.extractor.extractor_lists import ExtractorLists
 from views import post_process
 from utils.string_utilities import standardize_uri
 from utils.dict_utilities import majority_voting_merge
-from rdflib import Graph as G, URIRef, RDFS, XSD, Literal, SKOS, OWL, DCTERMS
+from rdflib import Graph as G, URIRef, RDFS, XSD, Literal, SKOS, OWL, PROV
 from datetime import timezone
 from update import Updater
+from llm.llm_connection import LLMConnection
+from collections import defaultdict
 
 properties = Properties()
 
@@ -25,7 +29,7 @@ class MergeURIs():
     def __init__(self,
                  input_ontologies: list[str],
                  output_ontology: str,
-                 community_views: list[str]):
+                 community_views: list[str] = []):
 
         self._graph = Graph(input_ontologies)
         self._output_ontology = output_ontology
@@ -87,28 +91,34 @@ class MergeURIs():
         for synset in synsets:
             synset_dicts = []
             all_uris_in_synset = []
+            labels_by_source = defaultdict(set)
             for s in synset:
                 all_uris_in_synset.append(s.uri)
                 d = dict()
+                s.add_source_to_attributes()
+
                 for key in s._data:
-                    return_language = False
-                    if key in [SKOS.altLabel, SKOS.prefLabel]:
-                        return_language = True
+                    is_label = key in [properties.convert_attr(p) for p in properties._KEEP_PROVENANCE]
                     d[key] = s.get_values_for(key,
                                               extend_to_synonyms = False,
-                                              return_language = return_language)
+                                              return_raw_value = not is_label)
                 synset_dicts.append(d)
             # synset_dicts = [s._data for s in synset]
-            data = majority_voting_merge(synset_dicts)
-            pref_label = data[SKOS.prefLabel]
-            if type(pref_label) == tuple and len(pref_label) == 2:
-                pref_label = pref_label[0]
-            term = standardize_uri(pref_label)
+            data = majority_voting_merge(synset_dicts) # TODO merge the labels that are the same, adding more provenances.
+
+            old_pref_label = data.get(properties.label)
+            pref_label = LLMConnection.generate_label_for_synset(synset = synset,
+                                                                 merged_data = data,
+                                                                 from_cache = True)
+            data[properties.alt_label].add(old_pref_label)
+            data[properties.label] = pref_label
+            term = standardize_uri(str(pref_label)) # TODO replace this by a better term generation strategy
             #while term in all_terms:
             #    term += "-bis"
             #all_terms.append(term)
             for uri in all_uris_in_synset:
-                term_by_synonym_uri[str(uri)] = term
+                term_by_synonym_uri[uri] = term
+                term_by_synonym_uri[pref_label] = term
             entity = properties.OBS[term]
             for property, values in data.items():
                 if property in self.IGNORE_PROPERTIES:
@@ -130,27 +140,22 @@ class MergeURIs():
                         pass
                     elif type(value) == Literal:
                         pass
-                    else:
+                    elif type(value) != Value:
                         if datatype != XSD.string:
                             if datatype == XSD.dateTime:
-                                if type(value) == tuple:
-                                    value = value[0]
+                                #if type(value) == tuple:
+                                #    value = value[0]
                                 # dt = dateparser.isoparse(str(value)).astimezone(timezone.utc)
                                 dt = value.astimezone(timezone.utc)
                                 value = Literal(dt.isoformat(), datatype=XSD.dateTime)
                             elif datatype == URIRef:
-                                value = properties.OBS[value]
+                                value = properties.OBS[standardize_uri(value)]
                             else:
                                 value = Literal(value, datatype = datatype)
                         else:
-                            if type(value) == tuple and len(value) == 2:
-                                lang = value[1]
-                                if lang:
-                                    value = Literal(value[0], lang = lang)
-                                else:
-                                    value = Literal(value[0], datatype = datatype)
-                            else:
-                                value = Literal(value, datatype = datatype)
+                            value = Literal(value, datatype = XSD.string)
+                    else:
+                        value = value.get_value_node(self._output_graph)
                     property = properties.convert_attr(property)
                     self._output_graph.add((entity, property, value))
         self._term_by_synonym_uri = term_by_synonym_uri
@@ -158,21 +163,19 @@ class MergeURIs():
 
 
     def _fix_internal_link(self):
-        attrs = [DCTERMS.hasPart, DCTERMS.isPartOf]
+        attrs = [properties.has_part, properties.is_part_of]
         for attr in attrs:
             for old_obj, new_obj in self._term_by_synonym_uri.items():
-                old_obj = URIRef(old_obj)
                 for subj, pred, _ in self._output_graph.triples((None, attr, old_obj)):
                     self._output_graph.remove((subj, pred, old_obj))
-                    self._output_graph.add((subj, pred, Properties().OBS[new_obj]))
+                    self._output_graph.add((subj, pred, properties.OBS[new_obj]))
             # Remove Wikidata links (for WD hasPart/isPartOf that was not linked to any authoritative list)
             for subj, pred, obj in self._output_graph.triples((None, attr, None)):
                 if "wikidata#" in str(obj):
-                    # print("removing:", obj)
                     self._output_graph.remove((subj, pred, obj))
                 if "wikidata#" in str(subj):
-                    # print("removing:", subj)
                     self._output_graph.remove((subj, pred, obj))
+            # TODO do this for every list that is not in community here (instead of in post process)
 
 
     def write_ttl(self):
@@ -184,7 +187,7 @@ class MergeURIs():
         for s, p, o in self._graph.triples((None, RDFS.subClassOf, None)):
             self._output_graph.add((s, p, o))
         # source lists
-        for s, _, _ in self._graph.triples((None, None, properties.OBS["facility-list"])):
+        for s, _, _ in self._graph.triples((None, None, PROV.Entity)):
             for _, p, o in self._graph.triples((s, None, None)):
                 self._output_graph.add((s, p, o))
 
@@ -195,7 +198,7 @@ class MergeURIs():
 
 def main(input_ontology,
          output_ontology,
-         community_views: list[str]):
+         community_views: list[str] = []):
     """
     Args:
         community_views: list of communities that should be considered as authoritative for this extraction. Values: IHDEA, IVOA, IPDA, OGC.
@@ -228,6 +231,16 @@ if __name__ == "__main__":
                         required = False,
                         default = "output_onto_portal.ttl",
                         help = "Output ontology filename (OntoPortal format)")
+
+    parser.add_argument("-c",
+                        "--community-views",
+                        dest = "community_views",
+                        type = str,
+                        nargs = "*",
+                        choices = set(Updater.SOURCE_BY_PRIMARY_COMMUNITY),
+                        help = "Consider lists for which the primary community (or alliance) is amongst this list as authoritative, and ignore synonym sets (or lonely entities) that did not match with any entity in these lists.")
+
     args = parser.parse_args()
     main(args.input_ontology,
-         args.output_ontology)
+         args.output_ontology,
+         args.community_views)
