db5ad2463f9b28a4244e90e8221cb23d42bc76fc
Sazuna <liza.fretel@gmail.com>
2026-07-13 19:14:26 +0200

    Make release + merge_uris: create json intermediate file in output folder.

---
+++ make_release.py
@@ -0,0 +1,10 @@
+"""
+Create a new release from a previous release.
+"""
+
+
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
--- src/views/merge_uris.py
+++ src/views/merge_uris.py
@@ -6,6 +6,8 @@ Author:
     Liza Fretel (liza.fretel@obspm.fr)
 """
 import argparse
+import json
+import atexit
 
 from graph.graph import Graph
 from graph.properties import Properties
@@ -15,11 +17,13 @@ from graph.extractor.extractor_lists import ExtractorLists
 from views import post_process
 from utils.string_utilities import standardize_uri
 from utils.dict_utilities import majority_voting_merge
+from config import TERM_LABEL_DEF_FILE
 from rdflib import Graph as G, URIRef, RDFS, XSD, Literal, SKOS, OWL, PROV
 from datetime import timezone
 from update import Updater
 from llm.llm_connection import LLMConnection
 from collections import defaultdict
+from uuid import uuid1
 
 properties = Properties()
 
@@ -92,6 +96,9 @@ class MergeURIs():
             synset_dicts = []
             all_uris_in_synset = []
             labels_by_source = defaultdict(set)
+
+            # Get synset's data into dicts for each member of the synset,
+            # then merge them with majority voting
             for s in synset:
                 all_uris_in_synset.append(s.uri)
                 d = dict()
@@ -106,16 +113,25 @@ class MergeURIs():
             # synset_dicts = [s._data for s in synset]
             data = majority_voting_merge(synset_dicts) # TODO verify that provenances are merged correctly
 
+            # Post-processing of the synset: generate term (IVOA-friendly URI) & pref_label & definition
+            # pref label
             old_pref_label = data.get(properties.label)
-            pref_label = LLMConnection.generate_label_for_synset(synset = synset,
-                                                                 merged_data = data,
-                                                                 from_cache = True)
+            old_definition = data.get(properties.definition)
+            term, pref_label, definition  = self._get_term_label_def(synset_dicts)
+
             data[properties.alt_label].add(old_pref_label)
             data[properties.label] = pref_label
-            term = standardize_uri(str(pref_label)) # TODO replace this by a better term generation strategy
+            data[properties.description].update(old_definition) # FIXME check that the type is correct (expected bug)
+
+            # definition
+            # TODO use the one from post_processing
+
+            # term
+            # term = standardize_uri(str(pref_label)) # TODO replace this by a better term generation strategy
             #while term in all_terms:
             #    term += "-bis"
             #all_terms.append(term)
+
             for uri in all_uris_in_synset:
                 term_by_synonym_uri[uri] = term
                 term_by_synonym_uri[pref_label] = term
@@ -162,6 +178,79 @@ class MergeURIs():
         self._fix_internal_link()
 
 
+    # Label by synset ID & Synset ID by source
+    _term_label_def_dict = {}
+    def _load_term_label_def(self):
+        if not self._term_label_def_dict and TERM_LABEL_DEF_FILE.exists():
+            with open(TERM_LABEL_DEF_FILE, "r") as file:
+                self._term_label_def_dict = json.load(file)
+        atexit.register(self._save_term_label_def)
+
+
+    def _save_term_label_def(self):
+        with open(TERM_LABEL_DEF_FILE, "r") as file:
+            json.dump(self._term_label_def_dict)
+
+
+    def _get_term_label_def(self,
+                            synset_member_uris: list[str],
+                            synset_dicts: list[dict],
+                            data: dict,
+                            all_terms: list[str]) -> tuple[str, str, str]:
+        """
+        Find a potential term for a synonym set by
+        finding acronyms, aperture (m), and checking whether
+        it exists in the all_terms history. If it does, it
+        will ask the LLM to generate a term.
+        Save in dict (and load above for term & definition & label)
+        This dictionary can be modified by the user (it should be saved in the data folder, not in cache)
+        This dict is then used to:
+            - be loaded from any of the URIs in the synset (above)
+            - provide the pref label, definition & term for this synset
+            - if the synsets are not the same anymore as in this file (split into two synsets in the cache), then we choose one of them and set one as deprecated with use instead to the other one.)
+
+
+        Args:
+            synset_uris: URIs of source (as in linked.ttl)
+            data: merged dict
+            all_terms: history of all generated terms
+        """
+        if not self._term_label_def_dict:
+            self._load_synset_file()
+
+        term, pref_label, definition = "", "", ""
+        for syn_uri in synset_member_uris:
+            synset_id = self._term_label_def_dict.get(syn_uri, None)
+            if synset_id:
+                break
+        values = self._term_label_def_dict.get(synset_id, dict())
+        term = values.get("term", "")
+        pref_label = values.get("label", "")
+        definition = values.get("definition", "")
+        method_str = values.get("method_str", "")
+        if not pref_label:
+            pref_label = LLMConnection.generate_label_for_synset(synset = synset_member_uris,
+                                                                 merged_data = data,
+                                                                 from_cache = True)
+            method_str += "label:LLM "
+
+        if not definition:
+            definition = LLMConnection.generate_definition_for_synset(synset = synset_member_uris,
+                                                                      merged_data = data,
+                                                                      from_cache = True)
+            method_str += "definition:LLM "
+
+        if not synset_id:
+            synset_id = str(uuid1())
+        for syn_uri in synset_member_uris:
+            self._term_label_def_dict[syn_uri] = synset_id
+            self._term_label_def_dict[synset_id] = {"term": term,
+                                                    "label": pref_label,
+                                                    "definition": definition,
+                                                    "method": method_str.strip()}
+        return term, pref_label, definition
+
+
     def _fix_internal_link(self):
         attrs = [properties.has_part, properties.is_part_of]
         for attr in attrs:
@@ -176,6 +265,21 @@ class MergeURIs():
                 if "wikidata#" in str(subj):
                     self._output_graph.remove((subj, pred, obj))
             # TODO do this for every list that is not in community here (instead of in post process)
+
+
+    def _check_terms_unicity(self,
+                             terms: list[str]) -> list:
+        """
+        Check that terms are unique.
+
+        Returns:
+            a list of non-unique terms
+        """
+        duplicate = set()
+        for i, term in enumerate(terms):
+            if term in terms[:i]:
+                duplicate.add(term)
+        return duplicate
 
 
     def write_ttl(self):
